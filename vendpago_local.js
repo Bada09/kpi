6e131b16c02c3b81fa2e7da59345533ca54ef258
Bada09 <badadias@gmail.com>
2026-06-25 16:52:48 -0300

    feat: adiciona arquivos de dados locais para o GitHub Pages

---
+++ vendpago_local.js
@@ -0,0 +1,7 @@
+window.LAVAI_VENDPAGO_DATA = `
+Cliente;Máquina;Modelo;Fabricante;Pagamento;Produtos;Mola;Venda (R$);Preço (R$);Total;Código Promocional;Data;Hora;Nº Logico;NSU;Autorização;Tipo Cartão;Rede;Bandeira;Usuário;Nº Cartão;Matricula
+Estoque - LAVAI;3# Extra Rio Branco;VendPago (Nova);VendPago;CASHLESS;Lavadora;;16,99;Nao Informado;16,99;Nao utilizado;07/05/2026;10:49:14;;;;;VendPago;;;;123976325
+Estoque - LAVAI;31# Gestão PUQ - Perdizes;VendPago (Nova);VendPago;CASHLESS;Lavadora;;7;Nao Informado;7;Nao utilizado;07/05/2026;15:11:00;;;;;VendPago;;;;40920241459
+Estoque - LAVAI;3# Extra Rio Branco;VendPago (Nova);VendPago;CASHLESS;Lavadora;;16,99;Nao Informado;16,99;Nao utilizado;08/05/2026;12:09:15;;;;;VendPago;;;;123976325
+Estoque - LAVAI;21# Graça Teotônio;VendPago (Nova);VendPago;CASHLESS;Lavadora;;16;Nao Informado;16;Nao utilizado;11/05/2026;15:43:05;;;;;VendPago;;;;11122014151
+Estoque - LAVAI;12# Dia - Brooklin;VendPago (Nova);VendPago;CASHLESS;Lavadora;;16,99;Nao Informado;16,99;Nao utilizado;13/05/2026;18:41:16;;;;;VendPago;;;;123975390`;
