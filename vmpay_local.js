1ad7fe6f3e057f2ccce359887ac09ab6fbe2aabc
Bada09 <badadias@gmail.com>
2026-08-25 15:24:28 -0300

    Auto-update dashboard (root) - 2026-08-25 15:24:28

--- vmpay_local.js
+++ vmpay_local.js
@@ -10980,4 +10980,5 @@ Estoque - LAVAÍ;5# Extra Glicério;;VMPay;VMPay;Lavadora;1;14,99;Não Informado
 Estoque - LAVAÍ;8# Lopes - Jacu Pessego;;VMPay;VMPay;Secadora;2;14,99;Não Informado;14,99;Não utilizado;25/08/2026;13:31:26;;987941161;;Débito;Vertipay;Mastercard;;;
 Estoque - LAVAÍ;7# Dia - Maria Luiza;;VMPay;VMPay;Secadora;2;16,0;Não Informado;16,0;Não utilizado;25/08/2026;13:45:47;;987953955;;Débito;Vertipay;Mastercard;;;
 Estoque - LAVAÍ;5# Extra Glicério;;VMPay;VMPay;Lavadora;1;14,99;Não Informado;14,99;Não utilizado;25/08/2026;14:21:19;;987982631;;Débito;Vertipay;Visa;;;
-Estoque - LAVAÍ;5# Extra Glicério;;VMPay;VMPay;Lavadora;1;14,99;Não Informado;14,99;Não utilizado;25/08/2026;14:19:48;;987980907;;Débito;Vertipay;Visa;;;`;
+Estoque - LAVAÍ;5# Extra Glicério;;VMPay;VMPay;Lavadora;1;14,99;Não Informado;14,99;Não utilizado;25/08/2026;14:19:48;;987980907;;Débito;Vertipay;Visa;;;
+Estoque - LAVAÍ;11# Extra Barra Funda;;VMPay;VMPay;Lavadora;1;16,0;Não Informado;16,0;Não utilizado;25/08/2026;15:18:51;;988024230;;Débito;Vertipay;Visa;;;`;
