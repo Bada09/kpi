c983401a765d01af1201e6d7c987047da5f774ae
Bada09 <badadias@gmail.com>
2026-09-07 16:42:44 -0300

    Auto-update dashboard (root) - 2026-09-07 16:42:44

--- vmpay_local.js
+++ vmpay_local.js
@@ -11614,4 +11614,6 @@ Estoque - LAVAÍ;5# Extra Glicério;;VMPay;VMPay;Secadora;2;14,99;Não Informado
 Estoque - LAVAÍ;26# Condominio Grevilia;;VMPay;VMPay;Lavadora;1;16,99;Não Informado;16,99;Não utilizado;07/09/2026;16:11:07;;998996444;;Débito;Vertipay;Visa;;;
 Estoque - LAVAÍ;26# Condominio Grevilia;;VMPay;VMPay;Secadora;2;16,99;Não Informado;16,99;Não utilizado;07/09/2026;16:10:50;;998995152;;Débito;Vertipay;Visa;;;
 Estoque - LAVAÍ;4# Extra Palmeiras;;VMPay;VMPay;Secadora;2;14,99;Não Informado;14,99;Não utilizado;07/09/2026;16:18:36;;999004115;;Crédito;Vertipay;Mastercard;;;
-Estoque - LAVAÍ;7# Dia - Maria Luiza;;VMPay;VMPay;Secadora;2;16,0;Não Informado;16,0;Não utilizado;07/09/2026;16:32:04;;999015935;;Crédito;Vertipay;Visa;;;`;
+Estoque - LAVAÍ;7# Dia - Maria Luiza;;VMPay;VMPay;Secadora;2;16,0;Não Informado;16,0;Não utilizado;07/09/2026;16:32:04;;999015935;;Crédito;Vertipay;Visa;;;
+Estoque - LAVAÍ;14# Extra São Bernardo;;VMPay;VMPay;Secadora;2;14,99;Não Informado;14,99;Não utilizado;07/09/2026;16:39:52;;999023628;;Débito;Vertipay;Visa;;;
+Estoque - LAVAÍ;5# Extra Glicério;;VMPay;VMPay;Lavadora;1;14,99;Não Informado;14,99;Não utilizado;07/09/2026;16:33:53;;999017677;;Débito;Vertipay;Mastercard;;;`;
