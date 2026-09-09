666d1a49a482257bd435619a22fcf0d5ef9d21a0
Bada09 <badadias@gmail.com>
2026-09-09 18:26:42 -0300

    Auto-update dashboard (root) - 2026-09-09 18:26:42

--- vmpay_local.js
+++ vmpay_local.js
@@ -11751,4 +11751,5 @@ Estoque - LAVAÍ;4# Extra Palmeiras;;VMPay;VMPay;Lavadora;1;14,99;Não Informado
 Estoque - LAVAÍ;13# Extra São Caetano;;VMPay;VMPay;Lavadora;1;14,99;Não Informado;14,99;Não utilizado;09/09/2026;17:31:51;;1000694815;;Débito;Vertipay;Mastercard;;;
 Estoque - LAVAÍ;37# Mini Extra Rio Branco;;VMPay;VMPay;Secadora;2;14,99;Não Informado;14,99;Não utilizado;09/09/2026;17:41:42;;1000702665;;Débito;Vertipay;Elo;;;
 Estoque - LAVAÍ;7# Dia - Maria Luiza;;VMPay;VMPay;Lavadora;1;16,0;Não Informado;16,0;Não utilizado;09/09/2026;17:47:09;;1000708238;;Débito;Vertipay;Visa;;;
-Estoque - LAVAÍ;4# Extra Palmeiras;;VMPay;VMPay;Secadora;2;14,99;Não Informado;14,99;Não utilizado;09/09/2026;18:14:12;;1000732887;;Débito;Vertipay;Mastercard;;;`;
+Estoque - LAVAÍ;4# Extra Palmeiras;;VMPay;VMPay;Secadora;2;14,99;Não Informado;14,99;Não utilizado;09/09/2026;18:14:12;;1000732887;;Débito;Vertipay;Mastercard;;;
+Estoque - LAVAÍ;7# Dia - Maria Luiza;;VMPay;VMPay;Secadora;2;16,0;Não Informado;16,0;Não utilizado;09/09/2026;18:22:47;;1000739954;;Débito;Vertipay;Visa;;;`;
